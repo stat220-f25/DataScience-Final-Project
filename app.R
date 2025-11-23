6bf35bb64d8a871a74dda2d9c4af61b560062429
Cuong Chi Tran <chicuong04@gmail.com>
2025-11-23 00:51:45 -0600

    Much better Shiny app

--- app.R
+++ app.R
@@ -15,195 +15,320 @@ library(ggthemes)
 library(purrr)
 
 # You can also run code once here (nonreactive)
-min_dur_slp <- min(sleep_clean_name$sleep_duration, na.rm = TRUE)
-max_dur_slp <- max(sleep_clean_name$sleep_duration, na.rm = TRUE)
+x_input_label <- "Plot-X"
+y_input_label <- "Scatterplot-Y"
+
 quant_vars_slp <- sleep_clean_name |> keep(is.numeric)
 cat_vars_slp <- sleep_clean_name |> keep(is.factor)
 
-min_dur_std <- min(student_clean_name$sleep_hours, na.rm = TRUE)
-max_dur_std <- max(student_clean_name$sleep_hours, na.rm = TRUE)
 quant_vars_std <- student_clean_name |> keep(is.numeric)
 cat_vars_std <- student_clean_name |> keep(is.factor)
 
-min_dur_fct <- min(factors_clean_name$sleep_hours, na.rm = TRUE)
-max_dur_fct <- max(factors_clean_name$sleep_hours, na.rm = TRUE)
 quant_vars_fct <- factors_clean_name |> keep(is.numeric)
 cat_vars_fct <- factors_clean_name |> keep(is.factor)
 
-min_dur_f <- min(full_data_clean$sleep_duration, na.rm = TRUE)
-max_dur_f <- max(full_data_clean$sleep_duration, na.rm = TRUE)
 quant_vars_f <- full_data_clean |> keep(is.numeric)
 cat_vars_f <- full_data_clean |> keep(is.factor)
 
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- page_navbar(
+  title = "Exploring Sleep Data",
+  bg = "#2D89C8",
+  inverse = TRUE,
   
-  # Application title
-  titlePanel("Exploring Sleep Data"),
   
-  # Sleep data
-  inputPanel(
-    selectInput('xcol_slp', label = 'X_slp Variable', choices = colnames(quant_vars_slp)),
-    selectInput('ycol_slp', label = 'Y_slp Variable', choices = colnames(quant_vars_slp), selected = colnames(quant_vars_slp)[2]),
-    # selectInput('color_slp', label = 'Color by', choices = colnames(cat_vars_slp)),
-    sliderInput('size_slp', label = 'Point Size', min = 1, max = 10, value = 2),
-    sliderInput('duration_slp', label = 'Bounds for sleep duration', 
-                min = min_dur_slp, 
-                max = max_dur_slp, 
-                value = c(min_dur_slp, max_dur_slp)),
-    checkboxInput('background_slp', label = "Remove background", value = FALSE)
+  
+  nav_panel(
+    title = "Sleep Dataset", 
+    layout_sidebar(
+      sidebar = sidebar(
+        selectInput('xcol_slp', label = x_input_label, choices = colnames(quant_vars_slp)),
+        selectInput('ycol_slp', label = y_input_label, choices = colnames(quant_vars_slp), selected = colnames(quant_vars_slp)[2]),
+        
+        uiOutput("x_range_slider_slp"),
+        
+        sliderInput('size_slp', label = 'Point Size', min = 1, max = 10, value = 2),
+        checkboxInput('scatterline_slp', label = "Add best fit line", value = FALSE),
+        checkboxInput('background_slp', label = "Remove background", value = FALSE)
+      ),
+      # Show a plot of the selected relationship
+      plotOutput(outputId = "scatterPlot_slp"),
+      plotOutput(outputId = "histogram_slp"),
+    ),
+    
   ),
   
-  # Show a plot of the selected relationship
-  plotOutput(outputId = "scatterPlot_slp"),
-  plotOutput(outputId = "histogram_slp"),
-  
-  # ----------------------------------------------------------------
-  
-  # Student data
-  inputPanel(
-    selectInput('xcol_std', label = 'X_std Variable', choices = colnames(quant_vars_std)),
-    selectInput('ycol_std', label = 'Y_std Variable', choices = colnames(quant_vars_std), selected = colnames(quant_vars_std)[2]),
-    # selectInput('color_std', label = 'Color by', choices = colnames(cat_vars_std)),
-    sliderInput('size_std', label = 'Point Size', min = 1, max = 10, value = 2),
-    sliderInput('duration_std', label = 'Bounds for sleep hours', 
-                min = min_dur_std, 
-                max = max_dur_std, 
-                value = c(min_dur_std, max_dur_std)),
-    checkboxInput('background_std', label = "Remove background", value = FALSE)
+  nav_panel(
+    title = "Student Dataset", 
+    layout_sidebar(
+      sidebar = sidebar(
+        selectInput('xcol_std', label = x_input_label, choices = colnames(quant_vars_std)),
+        selectInput('ycol_std', label = y_input_label, choices = colnames(quant_vars_std), selected = colnames(quant_vars_std)[2]),
+        
+        uiOutput("x_range_slider_std"),
+        
+        sliderInput('size_std', label = 'Point Size', min = 1, max = 10, value = 2),
+        checkboxInput('scatterline_std', label = "Add best fit line", value = FALSE),
+        checkboxInput('background_std', label = "Remove background", value = FALSE)
+      ),
+      # Show a plot of the selected relationship
+      plotOutput(outputId = "scatterPlot_std"),
+      plotOutput(outputId = "histogram_std"),
+    ),
+    
   ),
   
-  # Show a plot of the selected relationship
-  plotOutput(outputId = "scatterPlot_std"),
-  plotOutput(outputId = "histogram_std"),
-  
-  # ----------------------------------------------------------------
-  
-  # Factors data
-  inputPanel(
-    selectInput('xcol_fct', label = 'X_fct Variable', choices = colnames(quant_vars_fct)),
-    selectInput('ycol_fct', label = 'Y_fct Variable', choices = colnames(quant_vars_fct), selected = colnames(quant_vars_fct)[2]),
-    # selectInput('color_fct', label = 'Color by', choices = colnames(cat_vars_fct)),
-    sliderInput('size_fct', label = 'Point Size', min = 1, max = 10, value = 2),
-    sliderInput('duration_fct', label = 'Bounds for sleep duration', 
-                min = min_dur_fct, 
-                max = max_dur_fct, 
-                value = c(min_dur_fct, max_dur_fct)),
-    checkboxInput('background_fct', label = "Remove background", value = FALSE)
+  nav_panel(
+    title = "Factors Dataset", 
+    layout_sidebar(
+      sidebar = sidebar(
+        selectInput('xcol_fct', label = x_input_label, choices = colnames(quant_vars_fct)),
+        selectInput('ycol_fct', label = y_input_label, choices = colnames(quant_vars_fct), selected = colnames(quant_vars_fct)[2]),
+        
+        uiOutput("x_range_slider_fct"),
+        
+        sliderInput('size_fct', label = 'Point Size', min = 1, max = 10, value = 2),
+        checkboxInput('scatterline_fct', label = "Add best fit line", value = FALSE),
+        checkboxInput('background_fct', label = "Remove background", value = FALSE)
+      ),
+      # Show a plot of the selected relationship
+      plotOutput(outputId = "scatterPlot_fct"),
+      plotOutput(outputId = "histogram_fct"),
+    ),
+    
   ),
   
-  # Show a plot of the selected relationship
-  plotOutput(outputId = "scatterPlot_fct"),
-  plotOutput(outputId = "histogram_fct"),
+  nav_panel(
+    title = "Joined Dataset", 
+    layout_sidebar(
+      sidebar = sidebar(
+        selectInput('xcol_f', label = x_input_label, choices = colnames(quant_vars_f)),
+        selectInput('ycol_f', label = y_input_label, choices = colnames(quant_vars_f), selected = colnames(quant_vars_f)[2]),
+        
+        uiOutput("x_range_slider_f"),
+        
+        sliderInput('size_f', label = 'Point Size', min = 1, max = 10, value = 2),
+        checkboxInput('scatterline_f', label = "Add best fit line", value = FALSE),
+        checkboxInput('background_f', label = "Remove background", value = FALSE)
+      ),
+      # Show a plot of the selected relationship
+      plotOutput(outputId = "scatterPlot"),
+      plotOutput(outputId = "histogram")
+    ),
+    
+  )
   
-  # ----------------------------------------------------------------
-  
-  # Full cleaned data
-  inputPanel(
-    selectInput('xcol_f', label = 'X Variable', choices = colnames(quant_vars_f)),
-    selectInput('ycol_f', label = 'Y Variable', choices = colnames(quant_vars_f), selected = colnames(quant_vars_f)[2]),
-    # selectInput('color_f', label = 'Color by', choices = colnames(cat_vars_f)),
-    sliderInput('size_f', label = 'Point Size', min = 1, max = 10, value = 2),
-    sliderInput('duration_f', label = 'Bounds for sleep duration', 
-                min = min_dur_f, 
-                max = max_dur_f, 
-                value = c(min_dur_f, max_dur_f)),
-    checkboxInput('background', label = "Remove background", value = FALSE)
-  ),
-  
-  # Show a plot of the selected relationship
-  plotOutput(outputId = "scatterPlot"),
-  plotOutput(outputId = "histogram")
+  #nav_spacer(),
+  #nav_menu(
+  #  title = "Links",
+  #  align = "right",
+  #  nav_item(tags$a("Posit", href = "https://posit.co")),
+  #  nav_item(tags$a("Shiny", href = "https://shiny.posit.co"))
+  #)
 )
+
 
 # Define server logic required to draw a scatterplot
 server <- function(input, output) {
+  #----------------------------------------------------------------------------------
   
-  # Sleep data
+  # Sleep Data
+  output$x_range_slider_slp <- renderUI({
+    selected_x_var_slp <- input$xcol_slp
+    data_vector <- sleep_clean_name[[selected_x_var_slp]]
+    
+    min_val <- min(data_vector, na.rm = TRUE)
+    max_val <- max(data_vector, na.rm = TRUE)
+    
+    sliderInput(
+      inputId = "x_range_slp",
+      label = paste("Bounds for", selected_x_var_slp),
+      min = min_val,
+      max = max_val,
+      value = c(min_val, max_val)
+    )
+  })
+  
   output$scatterPlot_slp <- renderPlot({
+    req(input$xcol_slp, input$x_range_slp, input$ycol_slp)
+    
     p <- sleep_clean_name |>
-      filter(between(sleep_duration, input$duration_slp[1], input$duration_slp[2])) |>
+      filter(between(.data[[input$xcol_slp]], input$x_range_slp[1], input$x_range_slp[2])) |>
       ggplot(aes(x = .data[[input$xcol_slp]], y = .data[[input$ycol_slp]])) +
       geom_point(aes(), size = input$size_slp) +
       scale_color_colorblind()
     if(input$background_slp) {
       p <- p + theme_bw()
     }
+    if(input$scatterline_slp) {
+      p <- p + geom_smooth(method = "lm", se = TRUE, color = "darkblue")
+    }
     
     p
   })
   
   output$histogram_slp <- renderPlot({
-    ggplot(sleep_clean_name, aes(x = .data[[input$xcol_slp]])) +
+    h <- sleep_clean_name |>
+      filter(between(.data[[input$xcol_slp]], input$x_range_slp[1], input$x_range_slp[2])) |>
+      ggplot(aes(x = .data[[input$xcol_slp]])) +
       geom_histogram() +
       scale_fill_colorblind()
+    if(input$background_slp) {
+      h <- h + theme_bw()
+    }
+    
+    h
   })
   
   # -------------------------------------------------------------------------------
   
   # Student data
+  output$x_range_slider_std <- renderUI({
+    selected_x_var_std <- input$xcol_std
+    data_vector <- student_clean_name[[selected_x_var_std]]
+    
+    min_val <- min(data_vector, na.rm = TRUE)
+    max_val <- max(data_vector, na.rm = TRUE)
+    
+    sliderInput(
+      inputId = "x_range_std",
+      label = paste("Bounds for", selected_x_var_std),
+      min = min_val,
+      max = max_val,
+      value = c(min_val, max_val)
+    )
+  })
+  
   output$scatterPlot_std <- renderPlot({
+    req(input$xcol_std, input$x_range_std, input$ycol_std)
+    
     p <- student_clean_name |>
-      filter(between(sleep_hours, input$duration_std[1], input$duration_std[2])) |>
+      filter(between(.data[[input$xcol_std]], input$x_range_std[1], input$x_range_std[2])) |>
       ggplot(aes(x = .data[[input$xcol_std]], y = .data[[input$ycol_std]])) +
       geom_point(aes(), size = input$size_std) +
       scale_color_colorblind()
     if(input$background_std) {
       p <- p + theme_bw()
     }
+    if(input$scatterline_std) {
+      p <- p + geom_smooth(method = "lm", se = TRUE, color = "darkblue")
+    }
     
     p
   })
   
   output$histogram_std <- renderPlot({
-    ggplot(student_clean_name, aes(x = .data[[input$xcol_std]])) +
+    h <- student_clean_name |>
+      filter(between(.data[[input$xcol_std]], input$x_range_std[1], input$x_range_std[2])) |>
+      ggplot(aes(x = .data[[input$xcol_std]])) +
       geom_histogram() +
       scale_fill_colorblind()
+    if(input$background_std) {
+      h <- h + theme_bw()
+    }
+    
+    h
   })
   
   # -------------------------------------------------------------------------------
   
   # factors data
+  output$x_range_slider_fct <- renderUI({
+    selected_x_var_fct <- input$xcol_fct
+    data_vector <- factors_clean_name[[selected_x_var_fct]]
+    
+    min_val <- min(data_vector, na.rm = TRUE)
+    max_val <- max(data_vector, na.rm = TRUE)
+    
+    sliderInput(
+      inputId = "x_range_fct",
+      label = paste("Bounds for", selected_x_var_fct),
+      min = min_val,
+      max = max_val,
+      value = c(min_val, max_val)
+    )
+  })
+  
   output$scatterPlot_fct <- renderPlot({
+    req(input$xcol_fct, input$x_range_fct, input$ycol_fct)
+    
     p <- factors_clean_name |>
-      filter(between(sleep_hours, input$duration_fct[1], input$duration_fct[2])) |>
+      filter(between(.data[[input$xcol_fct]], input$x_range_fct[1], input$x_range_fct[2])) |>
       ggplot(aes(x = .data[[input$xcol_fct]], y = .data[[input$ycol_fct]])) +
       geom_point(aes(), size = input$size_fct) +
       scale_color_colorblind()
     if(input$background_fct) {
       p <- p + theme_bw()
     }
+    if(input$scatterline_fct) {
+      p <- p + geom_smooth(method = "lm", se = TRUE, color = "darkblue")
+    }
     
     p
   })
   
   output$histogram_fct <- renderPlot({
-    ggplot(factors_clean_name, aes(x = .data[[input$xcol_fct]])) +
+    h <- factors_clean_name |>
+      filter(between(.data[[input$xcol_fct]], input$x_range_fct[1], input$x_range_fct[2])) |>
+      ggplot(aes(x = .data[[input$xcol_fct]])) +
       geom_histogram() +
       scale_fill_colorblind()
+    if(input$background_fct) {
+      h <- h + theme_bw()
+    }
+    
+    h
   })
   
   # -------------------------------------------------------------------------------
   
   # Full data
+  output$x_range_slider_f <- renderUI({
+    selected_x_var_f <- input$xcol_f
+    data_vector <- full_data_clean[[selected_x_var_f]]
+    
+    min_val <- min(data_vector, na.rm = TRUE)
+    max_val <- max(data_vector, na.rm = TRUE)
+    
+    sliderInput(
+      inputId = "x_range_f",
+      label = paste("Bounds for", selected_x_var_f),
+      min = min_val,
+      max = max_val,
+      value = c(min_val, max_val)
+    )
+  })
+  
   output$scatterPlot <- renderPlot({
+    req(input$xcol_f, input$x_range_f, input$ycol_f)
+    
     p <- full_data_clean |>
-      filter(between(sleep_duration, input$duration_f[1], input$duration_f[2])) |>
+      filter(between(.data[[input$xcol_f]], input$x_range_f[1], input$x_range_f[2])) |>
       ggplot(aes(x = .data[[input$xcol_f]], y = .data[[input$ycol_f]])) +
       geom_point(aes(), size = input$size_f) +
       scale_color_colorblind()
-    if(input$background) {
+    if(input$background_f) {
       p <- p + theme_bw()
+    }
+    if(input$scatterline_f) {
+      p <- p + geom_smooth(method = "lm", se = TRUE, color = "darkblue")
     }
     
     p
   })
   
   output$histogram <- renderPlot({
-    ggplot(full_data_clean, aes(x = .data[[input$xcol_f]])) +
+    h <- full_data_clean |>
+      filter(between(.data[[input$xcol_f]], input$x_range_f[1], input$x_range_f[2])) |>
+      ggplot(aes(x = .data[[input$xcol_f]])) +
       geom_histogram() +
       scale_fill_colorblind()
+    if(input$background_f) {
+      h <- h + theme_bw()
+    }
+    
+    h
   })
 }
 
